db408027709876f11eff0570efcf46ee37700442
zflcs <1491657576@qq.com>
2024-11-06 10:50:48 +0800

    Change elf loader.

--- modules/executor/src/loader.rs
+++ modules/executor/src/loader.rs
@@ -2,7 +2,6 @@ use core::{ptr::copy_nonoverlapping, str::from_utf8};
 
 use crate::link::real_path;
 use alloc::{
-    boxed::Box,
     string::{String, ToString},
     vec,
     vec::Vec,
@@ -18,105 +17,109 @@ use xmas_elf::program::SegmentData;
 
 /// 返回应用程序入口，用户栈底，用户堆底
 pub async fn load_app(
-    name: String,
+    mut name: String,
     mut args: Vec<String>,
     envs: &Vec<String>,
     memory_set: &mut MemorySet,
 ) -> AxResult<(VirtAddr, VirtAddr, VirtAddr)> {
-    if name.ends_with(".sh") {
-        args = [vec![String::from("busybox"), String::from("sh")], args].concat();
-        return Box::pin(load_app("busybox".to_string(), args, envs, memory_set)).await;
-    }
-    let elf_data = if let Ok(ans) = async_fs::api::read(name.as_str()).await {
-        ans
-    } else {
-        // exit(0)
-        info!("App not found: {}", name);
-        return Err(AxError::NotFound);
-    };
-    let elf = xmas_elf::ElfFile::new(&elf_data).expect("Error parsing app ELF file.");
-    if let Some(interp) = elf
-        .program_iter()
-        .find(|ph| ph.get_type() == Ok(xmas_elf::program::Type::Interp))
-    {
-        let interp = match interp.get_data(&elf) {
-            Ok(SegmentData::Undefined(data)) => data,
-            _ => panic!("Invalid data in Interp Elf Program Header"),
+    loop {
+        if name.ends_with(".sh") {
+            args = [vec![String::from("busybox"), String::from("sh")], args].concat();
+            name = "busybox".to_string();
+            continue;
+        }
+        let elf_data = if let Ok(ans) = async_fs::api::read(name.as_str()).await {
+            ans
+        } else {
+            // exit(0)
+            info!("App not found: {}", name);
+            return Err(AxError::NotFound);
         };
+        let elf = xmas_elf::ElfFile::new(&elf_data).expect("Error parsing app ELF file.");
+        if let Some(interp) = elf
+            .program_iter()
+            .find(|ph| ph.get_type() == Ok(xmas_elf::program::Type::Interp))
+        {
+            let interp = match interp.get_data(&elf) {
+                Ok(SegmentData::Undefined(data)) => data,
+                _ => panic!("Invalid data in Interp Elf Program Header"),
+            };
 
-        let interp_path = from_utf8(interp).expect("Interpreter path isn't valid UTF-8");
-        // remove trailing '\0'
-        let interp_path = interp_path.trim_matches(char::from(0)).to_string();
-        let real_interp_path = real_path(&interp_path).await;
-        args = [vec![real_interp_path.clone()], args].concat();
-        return Box::pin(load_app(real_interp_path, args, envs, memory_set)).await;
-    }
-    info!("load app args: {:?} name: {}", args, name);
-    let elf_base_addr = Some(0x400_0000);
-    warn!("The elf base addr may be different in different arch!");
-    // let (entry, segments, relocate_pairs) = parse_elf(&elf, elf_base_addr);
-    let entry = get_elf_entry(&elf, elf_base_addr);
-    let segments = get_elf_segments(&elf, elf_base_addr);
-    let relocate_pairs = get_relocate_pairs(&elf, elf_base_addr);
-    for segment in segments {
+            let interp_path = from_utf8(interp).expect("Interpreter path isn't valid UTF-8");
+            // remove trailing '\0'
+            let interp_path = interp_path.trim_matches(char::from(0)).to_string();
+            let real_interp_path = real_path(&interp_path).await;
+            args = [vec![real_interp_path.clone()], args].concat();
+            name = real_interp_path;
+            continue;
+        }
+        info!("load app args: {:?} name: {}", args, name);
+        let elf_base_addr = Some(0x400_0000);
+        warn!("The elf base addr may be different in different arch!");
+        // let (entry, segments, relocate_pairs) = parse_elf(&elf, elf_base_addr);
+        let entry = get_elf_entry(&elf, elf_base_addr);
+        let segments = get_elf_segments(&elf, elf_base_addr);
+        let relocate_pairs = get_relocate_pairs(&elf, elf_base_addr);
+        for segment in segments {
+            memory_set
+                .new_region(
+                    segment.vaddr,
+                    segment.size,
+                    false,
+                    segment.flags,
+                    segment.data.as_deref(),
+                    None,
+                )
+                .await;
+        }
+
+        for relocate_pair in relocate_pairs {
+            let src: usize = relocate_pair.src.into();
+            let dst: usize = relocate_pair.dst.into();
+            let count = relocate_pair.count;
+            unsafe { copy_nonoverlapping(src.to_ne_bytes().as_ptr(), dst as *mut u8, count) }
+        }
+
+        // Now map the stack and the heap
+        let heap_start = VirtAddr::from(USER_HEAP_BASE);
+        let heap_data = [0_u8].repeat(MAX_USER_HEAP_SIZE);
         memory_set
             .new_region(
-                segment.vaddr,
-                segment.size,
+                heap_start,
+                MAX_USER_HEAP_SIZE,
                 false,
-                segment.flags,
-                segment.data.as_deref(),
+                MappingFlags::READ | MappingFlags::WRITE | MappingFlags::USER,
+                Some(&heap_data),
                 None,
             )
             .await;
-    }
-
-    for relocate_pair in relocate_pairs {
-        let src: usize = relocate_pair.src.into();
-        let dst: usize = relocate_pair.dst.into();
-        let count = relocate_pair.count;
-        unsafe { copy_nonoverlapping(src.to_ne_bytes().as_ptr(), dst as *mut u8, count) }
-    }
-
-    // Now map the stack and the heap
-    let heap_start = VirtAddr::from(USER_HEAP_BASE);
-    let heap_data = [0_u8].repeat(MAX_USER_HEAP_SIZE);
-    memory_set
-        .new_region(
+        info!(
+            "[new region] user heap: [{:?}, {:?})",
             heap_start,
-            MAX_USER_HEAP_SIZE,
-            false,
-            MappingFlags::READ | MappingFlags::WRITE | MappingFlags::USER,
-            Some(&heap_data),
-            None,
-        )
-        .await;
-    info!(
-        "[new region] user heap: [{:?}, {:?})",
-        heap_start,
-        heap_start + MAX_USER_HEAP_SIZE
-    );
+            heap_start + MAX_USER_HEAP_SIZE
+        );
 
-    let auxv = get_auxv_vector(&elf, elf_base_addr);
+        let auxv = get_auxv_vector(&elf, elf_base_addr);
 
-    let stack_top = VirtAddr::from(USER_STACK_TOP);
-    let stack_size = MAX_USER_STACK_SIZE;
+        let stack_top = VirtAddr::from(USER_STACK_TOP);
+        let stack_size = MAX_USER_STACK_SIZE;
 
-    let (stack_data, stack_bottom) = get_app_stack_region(args, envs, auxv, stack_top, stack_size);
-    memory_set
-        .new_region(
+        let (stack_data, stack_bottom) = get_app_stack_region(args, envs, auxv, stack_top, stack_size);
+        memory_set
+            .new_region(
+                stack_top,
+                stack_size,
+                false,
+                MappingFlags::USER | MappingFlags::READ | MappingFlags::WRITE,
+                Some(&stack_data),
+                None,
+            )
+            .await;
+        info!(
+            "[new region] user stack: [{:?}, {:?})",
             stack_top,
-            stack_size,
-            false,
-            MappingFlags::USER | MappingFlags::READ | MappingFlags::WRITE,
-            Some(&stack_data),
-            None,
-        )
-        .await;
-    info!(
-        "[new region] user stack: [{:?}, {:?})",
-        stack_top,
-        stack_top + stack_size
-    );
-    Ok((entry, stack_bottom.into(), heap_start))
+            stack_top + stack_size
+        );
+        return Ok((entry, stack_bottom.into(), heap_start));
+    }
 }
