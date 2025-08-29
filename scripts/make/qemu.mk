80e4afb28ae5223905132a554648b5e28a2be3b9
rosy <lc_rosy@qq.com>
2025-08-29 15:47:42 +0800

    更改配置以正常运行

--- scripts/make/qemu.mk
+++ scripts/make/qemu.mk
@@ -1,6 +1,7 @@
 # QEMU arguments
 
-QEMU := ../taic-qemu/build/qemu-system-$(ARCH)
+# QEMU := ../taic-qemu/build/qemu-system-$(ARCH)
+QEMU := qemu-system-$(ARCH)
 ifeq ($(BUS), mmio)
   vdev-suffix := device
 else ifeq ($(BUS), pci)
